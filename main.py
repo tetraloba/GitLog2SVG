3a05c3c907318961e5030ea3b65730215fa910ee
tetraloba <tetraloba@gmail.com>
2026-09-17 21:45:14 +0900

    implement Mermaid to SVG and add <title> (Niobe)

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import subprocess
 from typing import override
 
+from bs4 import BeautifulSoup
+
 # REPO="../databases"
 REPO = "./tmp"
-OUTFILE = "out.md"
+OUT_MD = "out.md"
+OUT_MMD = "out.mmd"
+OUT_SVG = "out.svg"
+OUT_SVG_WITH_TITLE = "out_with_title.svg"
+USER_ID = subprocess.check_output(['id', '-u'], text=True).strip()
+GROUP_ID = subprocess.check_output(['id', '-g'], text=True).strip()
+DOCKER_MMDC = ["docker", "run", "--rm", "-u", f"{USER_ID}:{GROUP_ID}", "-v", ".:/data", "minlag/mermaid-cli"]
 
 class Node:
     def __init__(self, name = None, title = None, description = None):
@@ -86,8 +94,44 @@ for branch in branches.values():
     commit_hash = subprocess.check_output(['git', '-C', REPO, 'rev-parse', branch.name()], text=True).strip()
     edges.append(Edge(source=branch, target=commits[commit_hash]))  # Add edge from branch to commit
 
-with open(OUTFILE, 'w') as f:
-    f.write('```mermaid\ngraph\n')
-    for edge in edges:
-        f.write(f"    {edge}\n")
+# Generate Mermaid text
+
+mermaid_text = 'graph\n'
+for edge in edges:
+    mermaid_text += f"    {edge}\n"
+
+with open(OUT_MD, 'w') as f:
+    f.write('```mermaid\n')
+    f.write(mermaid_text)
     f.write('```\n')
+
+with open(OUT_MMD, 'w') as f:
+    header = """---
+config:
+  htmlLabels: false
+---
+"""
+    f.write(header)
+    f.write(mermaid_text)
+
+# Generate SVG from Mermaid using Docker
+
+subprocess.run(DOCKER_MMDC + ['-i', OUT_MMD, '-o', OUT_SVG])
+
+# Add title tags to the generated SVG
+
+name2title = {node.name(): node.title() for node in list(commits.values()) + list(branches.values())}
+with open(OUT_SVG, 'r', encoding='utf-8') as f:
+    soup = BeautifulSoup(f.read(), 'xml')
+# classに "node" を持つ <g> 要素をすべて抽出
+for node_g in soup.find_all('g', class_=lambda c: c and 'node' in c.split()):
+    # 内包する全テキスト（ネストされたtspanなど）を透過的に結合取得
+    commit_id = node_g.get_text(strip=True)
+    if commit_id in name2title:
+        if not node_g.find('title'):
+            # titleタグを生成して挿入
+            title_tag = soup.new_tag('title')
+            title_tag.string = name2title[commit_id]
+            node_g.append(title_tag)
+with open(OUT_SVG_WITH_TITLE, 'w', encoding='utf-8') as f:
+    f.write(str(soup))
